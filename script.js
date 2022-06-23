22ad825cca6f9b3f6d01dba098da2183fd6f0312
Alexei <alexeibradshaw12@gmail.com>
2022-06-23 20:33:43 +0100

    Created logic to add book items

--- script.js
+++ script.js
@@ -9,44 +9,60 @@ function Book(author = '', title = '', numberOfPages = 0, isRead = false) {
 };
 
 function addBookToLibrary() {
-    
-};
-
-myForm.addEventListener('submit', function(e) {
-    e.preventDefault();
     const formInfo = new FormData(myForm);
     const formPairs = {};
     for(const [name, value] of formInfo) {
         formPairs[name] = value;
     };
-    userBook = new Book(formPairs[author], formPairs[])
-})
+    userBook = new Book(formPairs.author, formPairs.title, formPairs.numberofpages, formPairs.hasread);
+    myLibrary.push(userBook);
+    clearChildren();
+    myLibrary.forEach(item => displayNewBook(item));
+};
 
-// function outputLibrary() {
-//     myLibrary.forEach(element => {
-//         const bookDiv = document.createElement('div');
-//         bookDiv.classList.add('book');
-//     });
-// };
+function displayNewBook(aBook) {
+    const books = document.querySelector('.books')
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('bookauthor');
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('booktitle');
+    const pagesDiv = document.createElement('div');
+    pagesDiv.classList.add('booknumberofpages');
+    const readDiv = document.createElement('div');
+    readDiv.classList.add('bookhasread');
+    authorDiv.textContent = aBook.author;
+    titleDiv.textContent = aBook.title;
+    pagesDiv.textContent = aBook.numberOfPages;
+    readDiv.textContent = aBook.numberOfPages;
+    bookDiv.appendChild(authorDiv);
+    bookDiv.appendChild(titleDiv);
+    bookDiv.appendChild(pagesDiv);
+    bookDiv.appendChild(readDiv);
+    books.appendChild(bookDiv);
+};
 
-// function addBookToLibrary() {
-//     const allItems = [...document.querySelectorAll('form > input')];
-//     allItems.forEach(item => console.log(item.textContent));
-// };
+function clearChildren() {
+    const books = document.querySelector('.books');
+    while(books.firstChild) {
+        books.removeChild(books.lastChild);
+    }
+};
+
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addBookToLibrary();
+});
 
 
 
-// const submitButton = document.querySelector('button');
-// submitButton.addEventListener('click', function() {
-//     addBookToLibrary();
-// })
 
 
 // addBookToLibrary();
 // const domBooks = document.querySelector('.books');
 // const bookDiv = document.createElement('div');
 // bookDiv.classList.add('book');
-// const varjack = new Book("V", "Varjack Paw", 15, true);
 // bookDiv.appendChild(document.createElement('div'));
 // bookDiv.firstChild.classList.add('author');
 // domBooks.appendChild(bookDiv);
